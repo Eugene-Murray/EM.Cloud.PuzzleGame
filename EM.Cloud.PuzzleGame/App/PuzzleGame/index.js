a5e3b73fb65a0dbb1fba464c4212f186864e38f2
Eugene-Murray <eugene.murray@gmail.com>
2014-03-02 19:30:32 +0000

    add puzzle game stuff

---
+++ EM.Cloud.PuzzleGame/App/PuzzleGame/index.js
@@ -0,0 +1,29 @@
+﻿define(['plugins/router', 'knockout'], function(router, ko) {
+    var childRouter = router.createChildRouter()
+        .makeRelative({
+            moduleId:'PuzzleGame',
+            fromParent:true
+        }).map([
+            { route: 'Admin',           moduleId: 'Admin/index',            title: 'Admin',                 type: 'intro',      nav: true},
+            { route: 'ChooseLocation',  moduleId: 'ChooseLocation/index',   title: 'Simple List',           type: 'intro',      nav: true },
+            { route: 'Easy',            moduleId: 'Easy/index',             title: 'Easy',                  type: 'intro',      nav: true},
+            { route: 'Hard',            moduleId: 'Hard/index',             title: 'Hard',                  type: 'intro',      nav: true },
+            { route: 'VeryHard',        moduleId: 'VeryHard/index',         title: 'Very Hard',             type: 'intro' ,     nav: true },
+            { route: 'Result',          moduleId: 'Result/index',           title: 'Result',                type: 'intro',      nav: true },
+            { route: 'RatingsBestTime', moduleId: 'RatingsBestTime/index',  title: 'Animated Transition',   type: 'intro',      nav: true }
+        ]).buildNavigationModel();
+
+    return {
+        router: childRouter,
+        introSamples: ko.computed(function() {
+            return ko.utils.arrayFilter(childRouter.navigationModel(), function(route) {
+                return route.type == 'intro';
+            });
+        }),
+        detailedSamples: ko.computed(function() {
+            return ko.utils.arrayFilter(childRouter.navigationModel(), function(route) {
+                return route.type == 'detailed';
+            });
+        })
+    };
+});
--- EM.Cloud.PuzzleGame/App/shell.js
+++ EM.Cloud.PuzzleGame/App/shell.js
@@ -12,7 +12,8 @@
                 { route: 'widgets',                     moduleId: 'widgets/index',          title: 'Widgets',           nav: true },
                 { route: 'master-detail',               moduleId: 'masterDetail/index',     title: 'Master Detail',     nav: true },
                 { route: 'knockout-samples*details', moduleId: 'ko/index', title: 'Knockout Samples', nav: true, hash: '#knockout-samples' },
-	            { route: 'mybreeze', moduleId: 'mybreeze/index', title: 'Breeze', nav: true }
+	            { route: 'mybreeze', moduleId: 'mybreeze/index', title: 'Breeze', nav: true },
+                { route: 'PuzzleGame', moduleId: 'PuzzleGame/index', title: 'Puzzle Game', nav: true },
             ]).buildNavigationModel()
               .mapUnknownRoutes('hello/index', 'not-found')
               .activate();
